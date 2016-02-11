3356e015a04a21cf20f5a550cf355fcd92ce21cf
doug.barthram <doug.barthram@lab49.com>
2016-02-11 17:21:56 +1100

    Screen workiong with React

---
+++ exchange-web/src/main/resources/index.js
@@ -0,0 +1,39 @@
+
+import React from "react";
+import dom from "react-dom";
+import PublicTrades from "./components/public-trades";
+import PrivateTrades from "./components/private-trades";
+import Snapshot from "./components/snapshot";
+
+class Exchange extends React.Component {
+
+    constructor() {
+        super();
+        this.instruments = [ "AMZN" ];
+    }
+
+    render() {
+        return (
+            <div>
+                {
+                    this.instruments.map(instrument => {
+                        return (
+                            <div>
+                                <Snapshot instrument={instrument}/>
+                                <hr/>
+                                <PublicTrades instrument={instrument}/>
+                                <hr/>
+                                <PrivateTrades instrument={instrument}/>
+                            </div>
+
+                        )
+                    })
+                }
+            </div>
+        )
+    }
+
+
+}
+
+dom.render(<Exchange/>, document.getElementById("app"));
