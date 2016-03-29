504c30ca71b7aac88eea01876683e5d87e40d45b
doug.barthram <doug.barthram@lab49.com>
2016-03-29 14:42:04 +1100

    docker build image

--- docker/build/build.sh
+++ docker/build/build.sh
@@ -1,2 +1,6 @@
 #!/bin/bash -eux
-docker run -it -v ${PWD}:/usr/src/mymaven -w /usr/src/mymaven maven:3.3.3-jdk-8 mvn package
+
+
+# docker run --privileged -it --name build -v /Users/dougbarthram/projects/ext/sorted/exchange:/src -v /Users/dougbarthram/.m2/repositoryD:/maven-repository --rm sortednet/build
+
+docker run --privileged --name build -it -v ${PWD}:/src -v ${HOME}/.m2/respositoryD:/root/.m2/repository 10.100.192.200:5000/exchangebuild
