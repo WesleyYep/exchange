a566ee6e161e2194f9e9e9993ee2328b9a357a45
sortednet <dug@sorted.net>
2016-05-22 11:43:45 +1000

    added xbuild.sh that uses a local .m2 dir to get the settings

---
+++ bin/xbuild.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -eux
+
+SRC=${PWD}
+CMD="mvn install"
+
+echo "Num args = $#"
+
+if [ $# -ge 1 ]; then
+	CMD=$*
+fi
+
+docker run --name build -it -v ${SRC}:/src -v ${HOME}/.m2/repository:/maven-repository -v ${HOME}/.m2:/root/.m2 -v /var/run/docker.sock:/var/run/docker.sock --rm exchangebuild $CMD
